13fbea432b25306bb989ee7a68becb1c397b9e57
Christopher Rudolph <Rudolchr@b-tu.de>
2021-06-28 16:46:48 +0200

    Fix initialisation

--- public/js/c/initialize.mjs
+++ public/js/c/initialize.mjs
@@ -1,14 +1,21 @@
-'use strict';
+//'use strict';
 // TODO: Replace the following with your app's Firebase project configuration
 if (!firebase.apps.length) {
     firebase.initializeApp({
-        apiKey:"AIzaSyDKETY-aByR9HQtf81ksO95kTns7QmBhe4",
-        authDomain:"freeopengym-69dae.firebaseapp.com",
-        projectId:"freeopengym-69dae"
+      apiKey: "AIzaSyDKETY-aByR9HQtf81ksO95kTns7QmBhe4",
+      authDomain: "freeopengym-69dae.firebaseapp.com",
+      projectId: "freeopengym-69dae",
+      storageBucket: "freeopengym-69dae.appspot.com",
+      messagingSenderId: "252705101391",
+      appId: "1:252705101391:web:a6f164f7cb435eee927c3a"
+        //apiKey:"AIzaSyDKETY-aByR9HQtf81ksO95kTns7QmBhe4",
+        //authDomain:"freeopengym-69dae.firebaseapp.com",
+        //projectId:"freeopengym-69dae"
     });
 }
 else {
 // if already initialized
+  console.log("WARN: already initialized");
     firebase.app();
 }
 
