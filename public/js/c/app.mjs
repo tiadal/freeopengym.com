ee0c8bccd6f02f3e147187f06ac8bd39284a894b
Christopher Rudolph <Rudolchr@b-tu.de>
2021-06-30 13:50:17 +0200

    generate Test data

--- public/js/c/app.mjs
+++ public/js/c/app.mjs
@@ -4,8 +4,47 @@ import Course from "../m/Course.mjs";
  * Generate test data for testing
  */
 //TODO generate data
-function generateTestData() {
+async function generateTestData() {
     console.log(`Nice try, but there's no test to be generated.`);
+    try{
+      let courseInstances = [
+        {
+          courseId: 1,
+          courseName: "Yoga in black",
+          categories: "Yoga",
+          price: 42,
+          description: "Nighttime yoga course"
+        },
+        {
+          courseId: 2,
+          courseName: "Red Socks football",
+          categories: "Football",
+          price: 0,
+          description: "Amateur football"
+        },
+        {
+          courseId: 3,
+          courseName: "Frisbee fun",
+          categories: "Frisbee",
+          price: 42,
+          description: ""
+        },
+        {
+          courseId: 4,
+          courseName: "Mythic gamers",
+          categories: "E-Sports",
+          price: 0,
+          description: "Searching for a partner for League of Legends duo queue. At least silver 5."
+        },
+      ];
+
+      await Promise.all(courseInstances.map(
+        courseRec => db.collection("courses").doc(courseRec.courseId.toString()).set(courseRec)
+      ));
+    } catch(e){
+      console.log("Error: " + e);
+    }
+
 }
 
 /**
