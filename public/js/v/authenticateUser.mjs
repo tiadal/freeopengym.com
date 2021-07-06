b4aaae416ea35a17a7c3a137dcc9db0bfaf8a21e
puchama1 <puchama1@b-tu.de>
2021-07-06 18:09:11 +0200

    sign in/sign out

--- public/js/v/authenticateUser.mjs
+++ public/js/v/authenticateUser.mjs
@@ -1,4 +1,3 @@
-//TODO
 setupUiByUserStatus = function () {
   // TODO Variablen
   const webPage = window.location.pathname;
@@ -6,8 +5,8 @@ setupUiByUserStatus = function () {
   const allowedPages =
   [
     "/",
-    "/intex.html",
-    "/authentificateUser.html"
+    "/index.html",
+    "/authenticateUser.html"
   ]
   console.log(webPage);
   console.log(loginManageEls);
@@ -30,8 +29,15 @@ setupUiByUserStatus = function () {
         console.log("registered");
         // TODO registered user handling
         if(webPage === "/index.html" || webPage === "/"){
-          loginManageEls[0].hidden = loginManageEls[1].hidden = true;
+          loginManageEls[0].hidden = true;
+          loginManageEls[1].hidden = false;
         }
+
+        // set and event handler for 'sign out' button
+        const signOutButton = loginManageEls[1].querySelector("button");
+        signOutButton.addEventListener("click", handleLogOut);
+        console.log(`Authenticated as 'registered with ${user.emailVerified ? '' : 'NO '}verified account' (${user.email})`);
+
       }
     } else { // if user is not 'registered' nor 'anonymous' (null)
       // sign in user as 'anonymous'
@@ -42,18 +48,91 @@ setupUiByUserStatus = function () {
 }
 
 setupSignInAndSignUp = function(){
-  // TODO
-  console.log("NYI");
+  console.log("Just work");
+  if(window.location.pathname === "/authenticateUser.html") {
+    const formEl = document.forms["User"],
+        btnSignIn = formEl.signin,
+        btnSignUp = formEl.signup;
+    // manage sign up event
+    btnSignUp.addEventListener("click", handleSignUpButton);
+    // manage sign in event
+    btnSignIn.addEventListener("click", handleSignInButton);
+    // neutralize the submit event
+    formEl.addEventListener("submit", function (e) {
+      e.preventDefault();
+    });
+  }
+}
+
+handleSignUpButton = async function (){
+  const formEl = document.forms["User"],
+      email = formEl.email.value,
+      password = formEl.password.value;
+  if (email && password) {
+    try {
+      // get 'anonymous' user data from IndexedDB
+      const userRef = await auth.currentUser;
+      // create credential providing email and password
+      const credential = firebase.auth.EmailAuthProvider.credential( email, password);
+      // create a 'registered' user merging credential with 'anonymous' user data
+      await userRef.linkWithCredential( credential);
+      // send verification email
+      await userRef.sendEmailVerification();
+      console.log (`User ${email} became 'Registered'`);
+      alert (`Account created ${email}.\n\nCheck your email for instructions to verify this account.`);
+      window.location.pathname = "/index.html";
+      console.log("Yay, you signed up!");
+    } catch (e) {
+      const divEl = document.getElementById("error"),
+          smallEl = divEl.querySelector("small");
+      smallEl.textContent = e.message;
+      divEl.hidden = false;
+      console.log("Well, you are NOT signed up :(");
+    }
+  } else {
+    console.log("There is no username or/and password.");
+  }
+}
+
+handleSignInButton = async function (){
+  const formEl = document.forms["User"],
+      email = formEl.email.value,
+      password = formEl.password.value;
+  if (email && password) {
+    try {
+      const signIn = await auth.signInWithEmailAndPassword( email, password);
+      if (signIn.user.emailVerified) {
+        console.log(`Granted access to user ${email}`);
+      }
+      window.location.pathname = "/index.html";
+      console.log(window.location.pathname);
+      console.log("Hurra, you are logged in!");
+    } catch (e) {
+      const divEl = document.getElementById("error"),
+          smallEl = divEl.querySelector("small");
+      smallEl.textContent = e.message;
+      divEl.hidden = false;
+    }
+  } else {
+    console.log("There is no username or/and password.");
+  }
 }
 
 handleVerifyEmail = function(){
   // TODO
   console.log("NYI");
+  return true;
 }
 
 handleLogOut = function(){
-  // TODO
-  console.log("NYI");
+  try {
+    auth.signOut();
+    window.location.pathname = "/index.html";
+    console.log("You have signed out");
+  } catch (e) {
+    console.error( e.message);
+  }
 }
 
 window.addEventListener("load", setupUiByUserStatus);
+window.addEventListener("load", setupSignInAndSignUp);
