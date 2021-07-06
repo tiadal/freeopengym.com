163109f48d0c0447d814f989e33b2b5ad81764c1
Christopher Rudolph <Rudolchr@b-tu.de>
2021-07-06 15:27:32 +0200

    User authentification linking and setupSide

--- public/js/v/authenticateUser.mjs
+++ public/js/v/authenticateUser.mjs
@@ -1,1 +1,59 @@
 //TODO
+setupUiByUserStatus = function () {
+  // TODO Variablen
+  const webPage = window.location.pathname;
+  const loginManageEls = document.querySelectorAll("header > #login-management >span");
+  const allowedPages =
+  [
+    "/",
+    "/intex.html",
+    "/authentificateUser.html"
+  ]
+  console.log(webPage);
+  console.log(loginManageEls);
+  console.log(allowedPages);
+
+  console.log("do smth");
+  //evaluate user authentication status
+  auth.onAuthStateChanged( async function (user) {
+    // if status is 'anonymous' or 'registered'
+    console.log("maybe");
+    if (user) {
+      console.log(user);
+      if (user.isAnonymous) { // if user is 'anonymous'
+        console.log("logged in anonymously");
+        // TODO anonymous user handling
+        if(webPage === "/index.html" || webPage === "/"){
+          loginManageEls[0].hidden = loginManageEls[1].hidden = false;
+        }
+      } else { // if user is 'registered'
+        console.log("registered");
+        // TODO registered user handling
+        if(webPage === "/index.html" || webPage === "/"){
+          loginManageEls[0].hidden = loginManageEls[1].hidden = true;
+        }
+      }
+    } else { // if user is not 'registered' nor 'anonymous' (null)
+      // sign in user as 'anonymous'
+      console.log("hier");
+      auth.signInAnonymously();
+    }
+  });
+}
+
+setupSignInAndSignUp = function(){
+  // TODO
+  console.log("NYI");
+}
+
+handleVerifyEmail = function(){
+  // TODO
+  console.log("NYI");
+}
+
+handleLogOut = function(){
+  // TODO
+  console.log("NYI");
+}
+
+window.addEventListener("load", setupUiByUserStatus);
