4728512290bef2779bd70072b80a513e2480cad9
Christopher Rudolph <Rudolchr@b-tu.de>
2021-07-24 15:10:55 +0200

    basic constraint validation nfor user

--- public/js/m/User.mjs
+++ public/js/m/User.mjs
@@ -1,3 +1,9 @@
+import {isIntegerOrIntegerString, isNonEmptyString} from "../../lib/util.mjs";
+import {NoConstraintViolation, MandatoryValueConstraintViolation,
+    RangeConstraintViolation, PatternConstraintViolation, UniquenessConstraintViolation,
+    IntervalConstraintViolation, ReferentialIntegrityConstraintViolation}
+    from "../../lib/errorTypes.mjs";
+
 class User{
   constructor({userid, username, password, dateOfBirth, bio/*, user_type, myCourses, joinedClasses, iFollow, followers*/}){
     this.userId = userId;
@@ -12,7 +18,38 @@ class User{
   }
 
   set userId(userId){
-    this._userId = userId;
+    const validationResult = checkUserId(userId);
+    if(validationResult instanceof NoConstraintViolation){
+      this._userId = userId;
+    } else{
+      throw validationResult;
+    }
+  }
+
+  static checkUserId(userId){
+    if(!userId){
+      return new MandatoryValueConstraintViolation("A UserId must be provided");
+    } else if(!isIntegerOrIntegerString(userId)){
+      return new RangeConstraintViolation("The UserId must be an unsigned Integer");
+    } else if(!parseInt(courseId < 1)){
+      return new RangeConstraintViolation("The UserId must be an unsigned Integer");
+    } else{
+      return new NoConstraintViolation();
+    }
+  }
+
+  static async checkUserIdAsId(userId){
+    let validationResult = User.checkUserId(courseId);
+    if(validationResult instanceof NoConstraintViolation){
+      let userDoc = await db.collection("users").doc(userId.toString()).get();
+      if(userDoc.exists){
+        validationResult = new UniquenessConstraintViolation("There is already a User with this ID");
+      } else {
+        validationResult = new NoConstraintViolation();
+      }
+    }
+
+    return validationResult;
   }
 
   get username(){
@@ -20,7 +57,20 @@ class User{
   }
 
   set username(username){
-    this._username = username;
+    const validationResult = User.checkUserName(username);
+    if(validationResult instanceof NoConstraintViolation){
+      this._username = username;
+    } else {
+      throw validationResult;
+    }
+  }
+
+  static checkUserName(userName){
+    if(!userName || !isNonEmptyString(userName)){
+      return new MandatoryValueConstraintViolation("A Username must be provided!");
+    } else {
+      return new NoConstraintViolation();
+    }
   }
 
   get password(){
@@ -28,7 +78,22 @@ class User{
   }
 
   set password(password){
-    this._password = password;
+    const validationResult = User.checkPassword();
+    if(validationResult instanceof NoConstraintViolation){
+      this._password = password;
+    } else {
+      throw validationResult;
+    }
+  }
+
+  static checkPassword(password){
+    if(!password){
+      return new MandatoryValueConstraintViolation("A password must be provided");
+    } else if(password.length < 6){
+      return new RangeConstraintViolation("The password must at least be 6 letters long");
+    } else {
+      return new NoConstraintViolation();
+    }
   }
 
   get birthday(){
@@ -36,7 +101,9 @@ class User{
   }
 
   set birthday(birthday){
-    this._birthday = birthday;
+    if(birthday){
+      this._birthday = birthday;
+    }
   }
 }
 
@@ -53,5 +120,5 @@ User.update = async function({userid, username, password, dateOfBirth, bio/*, us
 }
 
 User.retrieveAll = async function(){
-  
+
 }
